57a743bb117079d057991cf7b2285544ee3e8c3c
moustf <tepro.18@gmail.com>
2022-03-04 15:07:06 +0200

    Creating the variables which hold the elements close #5

---
+++ assets/javascript/index.js
@@ -0,0 +1,24 @@
+// * Declaring the variable that hold the elements.
+let resultDiv = document.querySelector(".result-screen");
+let cBtn = document.getElementsByClassName("c-btn")[0];
+let ceBtn = document.querySelector(".ce-btn");
+let modulus = document.getElementsByClassName("modulus")[0];
+let divisionBtn = document.querySelector("[value='/']");
+let multiplyBtn = document.querySelector("[value='*']");
+let modulusBtn = document.querySelector("[value='%']");
+let addBtn = document.querySelector("[value='+']");
+let subtractBtn = document.querySelector("[value='-']");
+let equalBtn = document.querySelector("[value='=']");
+let oneBtn = document.querySelector("[value='1']");
+let twoBtn = document.querySelector("[value='2']");
+let threeBtn = document.querySelector("[value='3']");
+let fourBtn = document.querySelector("[value='4']");
+let fiveBtn = document.querySelector("[value='5']");
+let sixBtn = document.querySelector("[value='6']");
+let sevenBtn = document.querySelector("[value='7']");
+let eightBtn = document.querySelector("[value='8']");
+let nineBtn = document.querySelector("[value='9']");
+let zeroBtn = document.querySelector("[value='0']");
+let dotBtn = document.querySelector("[value='.']");
+
+
