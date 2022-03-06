842286ad0ec0121ce7e3af17ee8cd27036718a93
moustf <tepro.18@gmail.com>
2022-03-06 16:03:09 +0200

    Enhacing the code 1 close #33

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -10,17 +10,7 @@ let modulusBtn = document.querySelector("[value='%']");
 let addBtn = document.querySelector("[value='+']");
 let subtractBtn = document.querySelector("[value='-']");
 let equalBtn = document.querySelector("[value='=']");
-let oneBtn = document.querySelector("[value='1']");
-let twoBtn = document.querySelector("[value='2']");
-let threeBtn = document.querySelector("[value='3']");
-let fourBtn = document.querySelector("[value='4']");
-let fiveBtn = document.querySelector("[value='5']");
-let sixBtn = document.querySelector("[value='6']");
-let sevenBtn = document.querySelector("[value='7']");
-let eightBtn = document.querySelector("[value='8']");
-let nineBtn = document.querySelector("[value='9']");
-let zeroBtn = document.querySelector("[value='0']");
-let dotBtn = document.querySelector("[value='.']");
+let btn = document.getElementsByClassName("btn");
 
 // * Getting the data from local storage.
 if (localStorage.getItem("math-operation")) {
@@ -32,22 +22,10 @@ if (localStorage.getItem("result")) {
 }
 
 // * Creating the function that is responsible for printing to the result screen and add event listeners.
-oneBtn.addEventListener("click", addToResultScreen);
-twoBtn.addEventListener("click", addToResultScreen);
-threeBtn.addEventListener("click", addToResultScreen);
-fourBtn.addEventListener("click", addToResultScreen);
-fiveBtn.addEventListener("click", addToResultScreen);
-sixBtn.addEventListener("click", addToResultScreen);
-sevenBtn.addEventListener("click", addToResultScreen);
-eightBtn.addEventListener("click", addToResultScreen);
-nineBtn.addEventListener("click", addToResultScreen);
-zeroBtn.addEventListener("click", addToResultScreen);
-dotBtn.addEventListener("click", addToResultScreen);
-divisionBtn.addEventListener("click", addToResultScreen);
-multiplyBtn.addEventListener("click", addToResultScreen);
-subtractBtn.addEventListener("click", addToResultScreen);
-addBtn.addEventListener("click", addToResultScreen);
-modulusBtn.addEventListener("click", addToResultScreen);
+
+for (let i = 0; i < btn.length; i++) {
+  btn[i].addEventListener("click", addToResultScreen);
+}
 
 function addToResultScreen(e) {
   operationsScreen.textContent += e.target.value;
@@ -58,8 +36,12 @@ function addToResultScreen(e) {
 cBtn.addEventListener("click", parseResultScreen);
 
 function parseResultScreen() {
-  operationsScreen.innerHTML = "";
-  resultDiv.innerHTML = "";
+  let pastOpText = operationsScreen.childNodes[0];
+  operationsScreen.removeChild(pastOpText);
+  operationsScreen.appendChild(document.createTextNode(""));
+  let pastRsText = resultDiv.childNodes[0];
+  resultDiv.removeChild(pastRsText);
+  resultDiv.appendChild(document.createTextNode(""));
   removeOperationFromLS();
   removeResultFromLS();
 }
@@ -92,7 +74,7 @@ function equation() {
         resultMul *= Number(multiply[i]);
       }
     }
-    resultDiv.textContent = resultMul;
+    resultDiv.textContent = "=" + resultMul;
     addResultToLS(resultMul);
   }
   if (resultText.includes("/")) {
@@ -102,7 +84,7 @@ function equation() {
         resultDivision /= Number(division[i]);
       }
     }
-    resultDiv.textContent = resultDivision;
+    resultDiv.textContent = "=" + resultDivision;
     addResultToLS(resultDivision);
   }
   if (resultText.includes("+")) {
@@ -112,7 +94,7 @@ function equation() {
         resultAdd += Number(add[i]);
       }
     }
-    resultDiv.textContent = resultAdd;
+    resultDiv.textContent = "=" + resultAdd;
     addResultToLS(resultAdd);
   }
   if (resultText.includes("-")) {
@@ -122,7 +104,7 @@ function equation() {
         resultSub -= Number(subtract[i]);
       }
     }
-    resultDiv.textContent = resultSub;
+    resultDiv.textContent = "=" + resultSub;
     addResultToLS(resultSub);
   }
   if (resultText.includes("%")) {
@@ -132,7 +114,7 @@ function equation() {
         resultMod %= Number(modulus[i]);
       }
     }
-    resultDiv.textContent = resultMod;
+    resultDiv.textContent = "=" + resultMod;
     addResultToLS(resultMod);
   }
   addOperationToLS(resultText);
